ec5dde687023c002e9c34a742390d0c556cbf2a1
ryu-mei <ryumei.sakaki06@gmail.com>
2024-01-14 20:45:04 +0900

    コールスタック

--- main.js
+++ main.js
@@ -2,7 +2,10 @@
 
 let val = 0;
 
-setTimeout( () => {val = 1;}, 1000);
+setTimeout( function task() {
+    val = 1;
+}, 0);
 
-// setTimeout関数で1000ミリ秒後に1を代入と指定しているので、console.logの結果は0となる。
+// コールスタックが空になるまで実行されないので、setTimeout関数の第二引数が0秒でもconsole.log実行後にsetTimeout関数が実行される。
+// console.logの結果は0
 console.log(val);
