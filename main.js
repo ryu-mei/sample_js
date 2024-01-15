44cabe72a50f7bcc4856860251010f95e6e3291b
ryu-mei <ryumei.sakaki06@gmail.com>
2024-01-15 21:02:05 +0900

    Promiseインスタンス

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 "use strict";
 
-let val = 0;
+// Promise
+let instance = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const rand = Math.floor(Math.random() * 11);
+        if (rand > 5) {
+            reject(rand);
+        } else {
+            resolve(rand);
+        }
+    }, 1000);
+});
 
-setTimeout( function task() {
-    val = 1;
-}, 0);
+instance = instance.then(value => {
+    console.log(`5以上の値[${value}]が渡ってきました。`);
+});
 
-// コールスタックが空になるまで実行されないので、setTimeout関数の第二引数が0秒でもconsole.log実行後にsetTimeout関数が実行される。
-// console.logの結果は0
-console.log(val);
+instance = instance.catch(value => {
+    console.log(`5未満の値[${value}]が渡ってきたためエラー表示`)
+});
+
+instance = instance.finally(() => {
+    console.log("処理を終了");
+});
+
+
+
+let promResolve, promReject;
+const prom = new Promise((resolve, reject) => {
+    promResolve = resolve;
+    promReject = reject;
+});
+
+console.log(prom);
+
+promResolve("引数");
+
+console.log(prom);
