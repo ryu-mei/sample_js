b969a34657b78e7b178794a2b766257a37cac585
ryu-mei <ryumei.sakaki06@gmail.com>
2024-01-16 07:32:51 +0900

    コールバック関数復習

--- main.js
+++ main.js
@@ -1,39 +1,13 @@
 "use strict";
 
-// Promise
-let instance = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        const rand = Math.floor(Math.random() * 11);
-        if (rand > 5) {
-            reject(rand);
-        } else {
-            resolve(rand);
-        }
-    }, 1000);
-});
+// コールバック
 
-instance = instance.then(value => {
-    console.log(`5以上の値[${value}]が渡ってきました。`);
-});
+let iwate = function () {
+    console.log("iwate")
+}
 
-instance = instance.catch(value => {
-    console.log(`5未満の値[${value}]が渡ってきたためエラー表示`)
-});
+let prefecture = function(callback) {
+    return callback;
+}
 
-instance = instance.finally(() => {
-    console.log("処理を終了");
-});
-
-
-
-let promResolve, promReject;
-const prom = new Promise((resolve, reject) => {
-    promResolve = resolve;
-    promReject = reject;
-});
-
-console.log(prom);
-
-promResolve("引数");
-
-console.log(prom);
+prefecture(iwate());
