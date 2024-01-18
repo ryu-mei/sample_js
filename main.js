3533f99e93559be70d1293e8e4c0155cb8e1bc77
ryu-mei <ryumei.sakaki06@gmail.com>
2024-01-18 21:27:08 +0900

    背景をクリックした時にCSS変更

--- main.js
+++ main.js
@@ -3,13 +3,14 @@
 
 // ここから下はポートフォリオ作成のためのプログラム
 {
-    // アイコンをクリックしたときにCSSを変えるプログラム
-    // ・アイコンをクリックする
-    // ・クラスを消す
+    // 背景をクリックしたときにCSSを変えるプログラム
+    const navBox = document.querySelector(".navBox");
+    navBox.addEventListener("click", displayNav);
 
-
-    // navクラスを消す
-    const getNav = document.querySelector(".nav");
-    getNav.classList.remove("nav");
-    console.log(getNav);
+    function displayNav() {
+        console.log("クリックされました")
+        navBox.classList.remove("navBox");
+        const getNav = document.querySelector(".nav");
+        getNav.classList.remove("nav");
+    }
 }
