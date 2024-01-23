2e6cab5af65e8b28939059c70c37f8053225e95c
ryu-mei <ryumei.sakaki06@gmail.com>
2024-01-24 08:21:47 +0900

    コンポーネントエラー

--- check_code/React/blog/app/page.js
+++ check_code/React/blog/app/page.js
@@ -1,12 +1,49 @@
 import { redirect } from "next/dist/server/api-utils";
 import Image from "next/image";
+import Header from "@/components/header";
+import Hero from "@/components/hero";
+import Footer from "@/components/footer";
+import { Main } from "next/document";
+
+// function EachPost() {
+//   return (
+//     <article>
+//       <a href="post.html">
+//         <h3>記事のタイトル</h3>
+//       </a>
+//     </article>
+//   )
+// }
+
+function EachPost(props) {
+  return (
+    <article>
+      <a href={props.url}>
+        <h3>{props.title}</h3>
+      </a>
+    </article>  
+  )
+}
+
+function Decoration(props) {
+  return (
+    <div style={{ color: "red" }}>
+      {props.children}
+    </div>
+  )
+}
+
 
 export default function Home() {
-  const subtitle = 'アウトプットしていくサイト'
+  const props1 = { title: "タイトル1", url: "post1.html"}
+  const props2 = { title: "タイトル2", url: "post2.html"}
   return  (
-    <div className="hero">
-      <h1 style={{color: 'red', fontSize: '80px'}}>CUBE</h1>
-      <p>{subtitle}</p>
-    </div>
+    <> 
+      <Header />
+      <main>
+        <Hero />
+      </main>
+      <Footer />
+    </>
   )
 }
