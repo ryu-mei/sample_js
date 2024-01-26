abd01a9edb7a102062a291911b04b39b5e26a3d5
ryu-mei <ryumei.sakaki06@gmail.com>
2024-01-26 21:05:44 +0900

    テキストをpropsでページごとに変更する

--- check_code/NextJs/blog/pages/blog/index.js
+++ check_code/NextJs/blog/pages/blog/index.js
@@ -1,5 +1,8 @@
 import Hero from 'components/hero'
 
 export default function Blog() {
-    return <Hero />
+    return <Hero 
+        title="Blog"
+        subtitle="Recent Posts"
+    />
 }
--- check_code/NextJs/blog/pages/index.js
+++ check_code/NextJs/blog/pages/index.js
@@ -1,5 +1,9 @@
 import Hero from 'components/hero'
 
 export default function Home() {
-  return <Hero />
+  return <Hero 
+    title="CUBE"
+    subtitle="アウトプットしていくサイト"
+    imageOn
+  />
 }
