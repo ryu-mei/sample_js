e37b1b728c5b3041237e2357af2490c3a4dce2b8
ryu-mei <ryumei.sakaki06@gmail.com>
2024-01-28 17:42:49 +0900

    流れるスライド完成

--- check_code/Swiper/index.js
+++ check_code/Swiper/index.js
@@ -1,16 +1,14 @@
-import Swiper, { Navigation, Pagination }from 'swiper';
-Swiper.use([Navigation, Pagination]);
-import 'swiper/swiper-bundle.css';
-
-
-const swiper = new Swiper(".swiper", {
-    loop:true,
-    pagination: {
-        el: '.swiper-pagenation'
-    },
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
+window.addEventListener("DOMContentLoaded", () => {
+  const sidewaySlider = new Swiper('.sideway-slider', {
+    direction: 'vertical',
+    loop: true,
+    loopedSlides: 5,
+    slidePerView: "auto",
+    speed: 2000,
+    direction: "horizontal",
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
     }
-});
+  });
+})
