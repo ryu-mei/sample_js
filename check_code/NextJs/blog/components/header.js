952c07ff97df6e323b9a01e78b058d3ebd1b25b9
ryu-mei <ryumei.sakaki06@gmail.com>
2024-02-05 08:13:33 +0900

    レイアウトの調整

--- check_code/NextJs/blog/components/header.js
+++ check_code/NextJs/blog/components/header.js
@@ -1,11 +1,14 @@
 import Logo from 'components/logo'
 import Nav from 'components/nav'
+import styles from 'styles/header.module.css'
 
 export default function Feader() {
     return (
         <header>
-            <Logo BoxOn />
-            <Nav />
+            <div className={styles.flexContainer}>
+                <Logo BoxOn />
+                <Nav />
+            </div>
         </header>
     )
 }
