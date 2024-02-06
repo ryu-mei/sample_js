70aef75a78de28fb11cc8080843b1afb53979725
ryu-mei <ryumei.sakaki06@gmail.com>
2024-02-07 06:51:53 +0900

    TypeScriptの勉強

---
+++ check_code/TsCode/sample.js
@@ -0,0 +1,16 @@
+// const obj = {a: 1};
+// // obj = {a: 2};
+// obj.a = 2;
+// console.log(obj.a);
+// const arr = [1, 2];
+// // arr = [3, 4];
+// arr.push(3);
+// console.log(arr);
+// const num: number = 123;
+// console.log(num);
+// console.log(window.innerWidth);
+console.log(greeting);
+var greeting = "こんにちは";
+var greeting;
+console.log(greeting);
+greeting = "こんにちは";
