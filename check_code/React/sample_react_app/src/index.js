eef6280a813415a160b2c5d363703a5050e6f5ef
ryu-mei <ryumei.sakaki06@gmail.com>
2024-02-19 20:19:18 +0900

    react useState

--- check_code/React/sample_react_app/src/index.js
+++ check_code/React/sample_react_app/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import Main from './component/main'
 import Header from './component/header';
 import Footer from './component/footer';
+import Form from './component/form';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
         <Header />
             <Main />
+            <Form />
         <Footer />
     </>
 
