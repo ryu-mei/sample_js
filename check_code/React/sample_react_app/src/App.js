b5d9ff1b206ec9dfacd2523989a3d019cafc58a1
ryu-mei <ryumei.sakaki06@gmail.com>
2024-02-21 07:27:03 +0900

    react_learn previous追加

--- check_code/React/sample_react_app/src/App.js
+++ check_code/React/sample_react_app/src/App.js
@@ -3,20 +3,43 @@ import { sculptureList } from "./data";
 
 export default function Gallery() {
     const [index, setIndex] = useState(0);
-
-    // index変数は何を返しているか？
-    console.log(index);
+    const [showMore, setShowMore] = useState(false);
     
-    function handleClick() {
-        setIndex(index + 1)
+    let hasPrev = index > 0;
+    let hasNext = index < sculptureList.length -1;
+
+    function handlePrevClick() {
+        if(hasPrev) {
+            setIndex(index -1);
+        }
+    }
+
+    function handleNextClick() {
+        if(hasNext) {
+            setIndex(index + 1);
+        }
+    }
+
+    function handleMoreClick() {
+        setShowMore(!showMore);
     }
 
     let sculpture = sculptureList[index];
     return (
         <>
-            <button onClick={handleClick}>
+            <button 
+            onClick={handlePrevClick}
+            disabled={!hasPrev}
+            >
+                Previous
+            </button>
+            <button 
+            onClick={handleNextClick}
+            disabled={!hasNext}
+            >
                 Next
             </button>
+
             <h2>
                 <i>{sculpture.name}</i>
                 by {sculpture.artist}
@@ -24,13 +47,14 @@ export default function Gallery() {
             <h3>
                 ({index + 1} of {sculptureList.length})
             </h3>
+            <button onClick={handleMoreClick}>
+                {showMore ? 'Hide' : 'Show'} details
+            </button>
+            {showMore && <p>{sculpture.description}</p>}
             <img 
                 src={sculpture.url}
                 alt={sculpture.alt}
             />
-            <p>
-                {sculpture.description}
-            </p>
         </>
     )
 }
