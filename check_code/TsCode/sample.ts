1bd60670c0a470339f868214ede58a75ac42e632
ryu-mei <ryumei.sakaki06@gmail.com>
2024-03-04 19:46:07 +0900

    typescript

--- check_code/TsCode/sample.ts
+++ check_code/TsCode/sample.ts
@@ -1,20 +1,5 @@
-// const obj = {a: 1};
-// // obj = {a: 2};
-// obj.a = 2;
-// console.log(obj.a);
+function increment(num: number) {
+    return num + 1;
+}
 
-// const arr = [1, 2];
-// // arr = [3, 4];
-// arr.push(3);
-// console.log(arr);
-
-// const num: number = 123;
-// console.log(num);
-// console.log(window.innerWidth);
-
-console.log(greeting);
-var greeting = "こんにちは";
-
-var greeting;
-console.log(greeting);
-greeting = "こんにちは";
+console.log(increment(999));
