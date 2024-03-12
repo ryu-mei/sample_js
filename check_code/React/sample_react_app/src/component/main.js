7c60cb408318436f3d139e4ba38f2d0ea53c9e05
ryu-mei <ryumei.sakaki06@gmail.com>
2024-03-13 06:33:49 +0900

    React clickHandler

--- check_code/React/sample_react_app/src/component/main.js
+++ check_code/React/sample_react_app/src/component/main.js
@@ -1,8 +1,12 @@
 export default function Main() {
+    const clickHandler = () => {
+      console.log('クリックされました。');
+    }
     return (
       <>
         <h1>hello</h1>
         <p>hello world</p>
+        <button onClick={clickHandler}>クリック</button>
       </>
     )
 }
