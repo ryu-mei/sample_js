10e92cac46c0c24826ee959cdcc5f65be2f3bc45
ryu-mei <ryumei.sakaki06@gmail.com>
2024-03-19 08:20:39 +0900

    react wether form

--- check_code/React/beginner/react-weather-app/src/components/Form.js
+++ check_code/React/beginner/react-weather-app/src/components/Form.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
-"https://api.weatherapi.com/v1/current.json?key=efecb59d3a13430c9ed212345241803&q=London&aqi=no"
+import axios from "axios";
 
 const Form = () => {
     const [city, setCity] = useState("");
+    const getWether = (e) => {
+        e.preventDefault();
+        axios.get("https://api.weatherapi.com/v1/current.json?key=efecb59d3a13430c9ed212345241803&q=London&aqi=no")
+        .then(res => console.log(res));
+    };
+    
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名" onChange={ e => setCity(e.target.value)} />
+        <input type="text" name="city" placeholder="都市名" onChange={ e => setCity(e.target.value)} />
             {city}
-            <button>Get Weather</button>
+            <button type="submit" onClick={getWether}>Get Weather</button>
         </form>
     )
 }
