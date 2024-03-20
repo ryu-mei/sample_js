009215069b39d8ea78e34f07cc5ada2b5e365483
ryu-mei <ryumei.sakaki06@gmail.com>
2024-03-21 07:38:03 +0900

    TypeScript 演習

--- check_code/TypeScript/increment.js
+++ check_code/TypeScript/increment.js
@@ -1,4 +1,14 @@
-function increment(num) {
-    return num + 1;
+// let age: number = 39;
+// let isDone: boolean = true;
+// let color: string = 'red';
+// const arr: string[] = [];
+// arr.push('foo');
+// arr.push(1);
+// console.log(arr);
+function printPoint(point) {
+    console.log("".concat(point.x));
+    console.log("".concat(point.y));
 }
-console.log(increment(999));
+printPoint({ x: 100, y: 100 });
+var age = 200;
+console.log(age);
--- check_code/TypeScript/increment.ts
+++ check_code/TypeScript/increment.ts
@@ -1,5 +1,59 @@
-function increment(num: number) {
-    return num + 1;
+// let age: number = 39;
+// let isDone: boolean = true;
+// let color: string = 'red';
+// const arr: string[] = [];
+// arr.push('foo');
+// arr.push(1);
+// console.log(arr);
+
+// function calc(isSum: boolean) {
+//     let a = 100;
+//     if (isSum) {
+//         let b = a + 1;
+//         return b;
+//     }
+// }
+
+// console.log(calc(true));
+
+// const user: { name: string; age?: number } = {
+//     name: 'foo',
+//     age: 20
+// }
+
+// console.log(user.name);
+
+// const user: any = {
+//     name: 'foo',
+//     age: 20
+// }
+
+// console.log(user.hello());
+
+// function sayHello(name: string): string {
+//     return `hello ${name}`
+// }
+
+// console.log(sayHello('foo'));
+
+// const age = 10;
+// const a = 'foo';
+// console.log(age.length);
+// console.log(a.length);
+
+type Point = {
+    x: number;
+    y: number;
 }
 
-console.log(increment(999));
+function printPoint(point: Point) {
+    console.log(`${point.x}`);
+    console.log(`${point.y}`);
+}
+
+printPoint({x: 100, y: 100});
+
+type Age = number;
+
+const age: Age = 200;
+console.log(age);
