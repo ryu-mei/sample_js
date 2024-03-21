1b46cfffbbc0a870d64e01ec5dcc9786a598691b
ryu-mei <ryumei.sakaki06@gmail.com>
2024-03-22 06:37:25 +0900

    react component2

--- check_code/React/sample_react_app/src/component/main.js
+++ check_code/React/sample_react_app/src/component/main.js
@@ -1,19 +1,11 @@
-function Recipe( {drinkers} ) {
-  return <p>個数{drinkers}</p>
+function Cup( {guest} ) {
+  return <p>ゲストは{guest}</p>
 }
 
 export default function Main() {
-    const clickHandler = () => {
-      alert('クリックされました。');
+    let cups = [];
+    for(let i = 1; i <= 12; i++) {
+      cups.push(<Cup key={i} guest={i} />);
     }
-    return (
-      <>
-        <Recipe drinkers={2}/>
-        <Recipe drinkers={3}/>
-        <Recipe drinkers={4}/>
-        <h1>hello</h1>
-        <p>hello world</p>
-        <button onClick={clickHandler}>クリック</button>
-      </>
-    )
+    return cups;
 }
