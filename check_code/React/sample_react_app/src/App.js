a496c4801e5aaff65ee3a1c9cd7833b5fa60597f
ryu-mei <ryumei.sakaki06@gmail.com>
2024-04-04 18:28:02 +0900

    react props

--- check_code/React/sample_react_app/src/App.js
+++ check_code/React/sample_react_app/src/App.js
@@ -1,60 +1,13 @@
-import { useState } from "react";
-import { sculptureList } from "./data";
-
-export default function Gallery() {
-    const [index, setIndex] = useState(0);
-    const [showMore, setShowMore] = useState(false);
-    
-    let hasPrev = index > 0;
-    let hasNext = index < sculptureList.length -1;
-
-    function handlePrevClick() {
-        if(hasPrev) {
-            setIndex(index -1);
-        }
-    }
-
-    function handleNextClick() {
-        if(hasNext) {
-            setIndex(index + 1);
-        }
-    }
-
-    function handleMoreClick() {
-        setShowMore(!showMore);
-    }
-
-    let sculpture = sculptureList[index];
+const App = ({ person, age, gender }) => {
     return (
         <>
-            <button 
-            onClick={handlePrevClick}
-            disabled={!hasPrev}
-            >
-                Previous
-            </button>
-            <button 
-            onClick={handleNextClick}
-            disabled={!hasNext}
-            >
-                Next
-            </button>
+            <p>{person}</p>
+            <p>{age}</p>
+            <p>{gender}</p>
 
-            <h2>
-                <i>{sculpture.name}</i>
-                by {sculpture.artist}
-            </h2>
-            <h3>
-                ({index + 1} of {sculptureList.length})
-            </h3>
-            <button onClick={handleMoreClick}>
-                {showMore ? 'Hide' : 'Show'} details
-            </button>
-            {showMore && <p>{sculpture.description}</p>}
-            <img 
-                src={sculpture.url}
-                alt={sculpture.alt}
-            />
+            <hr></hr>
         </>
-    )
-}
+    );
+};
+
+export default App;
--- check_code/React/sample_react_app/src/index.js
+++ check_code/React/sample_react_app/src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Main from './component/main'
-import Header from './component/header';
-import Footer from './component/footer';
-import Form from './component/form';
-import App from './App'
+import App from './App';
+import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-        <Header />
-            <Main />
-            <Form />
-            <App />
-        <Footer />
+        <App person="石井" age="18" gender="男" />
+        <App person="鈴木" age="20" gender="男" />
+        <App person="高橋" age="24" gender="男" />
     </>
-
 );
