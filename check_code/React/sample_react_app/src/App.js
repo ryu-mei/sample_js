2e5c954c814541bc2eadd695c315b4974c35d573
ryu-mei <ryumei.sakaki06@gmail.com>
2024-04-04 19:00:02 +0900

    react props 三項演算子

--- check_code/React/sample_react_app/src/App.js
+++ check_code/React/sample_react_app/src/App.js
@@ -1,9 +1,12 @@
-const App = ({ person, age, gender }) => {
+const App = ({ person, age, gender, member }) => {
     return (
         <>
             <p>{person}</p>
             <p>{age}</p>
             <p>{gender}</p>
+            <p>
+                <span>{member ? '会員' : '非会員'}</span>
+            </p>
 
             <hr></hr>
         </>
--- check_code/React/sample_react_app/src/index.js
+++ check_code/React/sample_react_app/src/index.js
@@ -6,8 +6,8 @@ import './style.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-        <App person="石井" age="18" gender="男" />
-        <App person="鈴木" age="20" gender="男" />
-        <App person="高橋" age="24" gender="男" />
+        <App person="石井" age="18" gender="男" member={true} />
+        <App person="鈴木" age="20" gender="男" member={false} />
+        <App person="高橋" age="24" gender="男" member={true} />
     </>
 );
