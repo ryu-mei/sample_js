89414d50bf1fc3cbd81f9a8d97640f10d424d170
ryumei <ryumei.sakaki06@gmail.com>
2024-04-08 17:56:08 +0900

    react demo

--- check_code/React/sample_react_app/src/App.js
+++ check_code/React/sample_react_app/src/App.js
@@ -1,15 +1,42 @@
 import { useState } from 'react';
-
+let nextId = 0;
 const App = () => {
-    const [value, setValue] = useState(0);
+    const [values, setValues] = useState('');
+    const [tasks, setTasks] = useState([]);
+
+    const changeHandler = (e) => {
+        setValues(e.target.value);
+    };
     const clickHandler = () => {
-        setValue(value + 1);
-        // console.log(value);
+        if (values !== '') {
+            setTasks([...tasks, { id: nextId++, name: values }]);
+        }
+        setValues('');
     };
     return (
         <>
-            <p>{value}</p>
+            <input value={values} onChange={changeHandler} />
             <button onClick={clickHandler}>ボタン</button>
+            <ul>
+                {tasks.map((task) => {
+                    return (
+                        <li key={task.id}>
+                            {task.name}
+                            <button
+                                onClick={() => {
+                                    setTasks(
+                                        tasks.filter((dtask) => {
+                                            return dtask.id !== task.id;
+                                        })
+                                    );
+                                }}
+                            >
+                                完了
+                            </button>
+                        </li>
+                    );
+                })}
+            </ul>
         </>
     );
 };
