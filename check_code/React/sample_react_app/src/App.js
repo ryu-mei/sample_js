33f462df5d7388738bc019a902b187676c4412e2
ryumei <ryumei.sakaki06@gmail.com>
2024-04-10 18:09:42 +0900

    react 野球クイズ

--- check_code/React/sample_react_app/src/App.js
+++ check_code/React/sample_react_app/src/App.js
@@ -1,23 +1,57 @@
 import { useState } from 'react';
 
+function submitForm(answer) {
+    return new Promise((resolve, reject) => {
+        let incorrect = answer !== '岩手県';
+        if (incorrect) {
+            reject(new Error('間違いですもう一度入力してください。'));
+        } else {
+            resolve();
+        }
+    });
+}
+
 const App = () => {
-    const personObj = { name: 'otani', position: 'pitchar' };
-    const [person, setPerson] = useState(personObj);
-    const changeName = (e) => {
-        setPerson({ ...person, name: e.target.value });
-    };
-    const changePosition = (e) => {
-        setPerson({ ...person, position: e.target.value });
-    };
+    const [answer, setAnswer] = useState('');
+    const [error, setError] = useState(null);
+    const [status, setStatus] = useState('typing');
+    if (status === 'success') {
+        return <h1>正解です。</h1>;
+    }
+    async function handleSubmit(e) {
+        e.preventDefault();
+        setStatus('submitting');
+        try {
+            await submitForm(answer);
+            setStatus('success');
+        } catch (err) {
+            setStatus('typing');
+            setError(err);
+        }
+    }
+
+    function handleTextareaChange(e) {
+        setAnswer(e.target.value);
+    }
     return (
         <>
-            <h3>{person.name}</h3>
-            <h3>{person.position}</h3>
+            <h3>野球クイズ</h3>
+            <p>大谷翔平選手の出身地は何県でしょう？</p>
 
-            <label>名前</label>
-            <input type="text" onChange={changeName} />
-            <label>ポジション</label>
-            <input type="text" onChange={changePosition} />
+            <form onSubmit={handleSubmit}>
+                <textarea
+                    value={answer}
+                    onChange={handleTextareaChange}
+                    disabled={status === 'submitting'}
+                />
+                <br />
+                <button
+                    disabled={answer.length === 0 || status === 'submitting'}
+                >
+                    答える
+                </button>
+                {error !== null && <p className="Error">{error.message}</p>}
+            </form>
         </>
     );
 };
