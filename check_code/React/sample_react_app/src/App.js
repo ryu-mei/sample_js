26cd243392a022a0eb1cc7e41bc2e8578823f1c8
ryumei <ryumei.sakaki06@gmail.com>
2024-04-16 06:27:42 +0900

    react send message part1

--- check_code/React/sample_react_app/src/App.js
+++ check_code/React/sample_react_app/src/App.js
@@ -1,36 +1,67 @@
 import { useState } from 'react';
 
 const App = () => {
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-    const pointerMove = (e) => {
-        // console.log(e.clientX);
-        setPosition({
-            x: e.clientX,
-            y: e.clientY,
+    const [text, setText] = useState({ name: '', message: '' });
+    const [isSending, setIsSending] = useState(false);
+    const [isSent, setIsSent] = useState(false);
+
+    const inputChange = (e) => {
+        setText({ ...text, name: e.target.value });
+        // setText({ name: e.target.value, message: text.message });
+    };
+    const textareaChange = (e) => {
+        // setText({ name: text.name, message: e.target.value });
+        setText({ ...text, message: e.target.value });
+    };
+    const sendMessage = (text) => {
+        return new Promise((resolve) => {
+            setTimeout(resolve, 2000);
         });
     };
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        setIsSending(true);
+        await sendMessage(text);
+        setIsSending(false);
+        setIsSent(true);
+    }
+
+    if (isSent) {
+        return (
+            <>
+                <p>{text.message}</p>
+                <img
+                    src="./images/baseball-7985432_640.jpg"
+                    alt="baseball"
+                ></img>
+                <h1>{text.name}さん応援ありがとうございます！</h1>
+            </>
+        );
+    }
+
     return (
-        <div
-            onPointerMove={pointerMove}
-            style={{
-                position: 'relative',
-                width: '100vw',
-                height: '100vh',
-            }}
-        >
-            <div
-                style={{
-                    position: 'absolute',
-                    backgroundColor: 'red',
-                    borderRadius: '50%',
-                    transform: `translate(${position.x}px,${position.y}px)`,
-                    left: -10,
-                    top: -10,
-                    width: 20,
-                    height: 20,
-                }}
-            ></div>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <p>大谷翔平選手に応援メッセージをお願いします！</p>
+            <label>お名前</label>
+            <input
+                disabled={isSending}
+                value={text.name}
+                onChange={inputChange}
+            />
+            <br />
+            <textarea
+                disabled={isSending}
+                value={text.message}
+                onChange={textareaChange}
+                placeholder={'メッセージを入力してください'}
+            />
+            <br />
+            <button disabled={isSending} type="submit">
+                メッセージを送る
+            </button>
+            {isSending && <p>Sending...</p>}
+        </form>
     );
 };
 
