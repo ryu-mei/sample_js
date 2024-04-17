6953b70d85183c4b9f7ffee221054f1e4719b998
ryumei <ryumei.sakaki06@gmail.com>
2024-04-18 07:25:46 +0900

    react baseball position part2

--- check_code/React/sample_react_app/src/App.js
+++ check_code/React/sample_react_app/src/App.js
@@ -1,34 +1,21 @@
 import { useState } from 'react';
 
 const App = () => {
-    const [position, setPosition] = useState('');
-    const [battingOrder, setBattingOrder] = useState('');
+    const [playPosition, setPlayPosition] = useState('ピッチャー');
 
-    const handlePositionChange = (e) => {
-        setPosition(e.target.value);
-    };
-
-    const handleBattingOrder = (e) => {
-        setBattingOrder(e.target.value);
-    };
-
+    function handlePositionChange(e) {
+        setPlayPosition(e.target.value);
+    }
+    // console.log(playPosition);
     return (
         <>
-            <p>今日のあなたのポジションと打順を入力してください</p>
-            <label>
-                ポジション
-                <input value={position} onChange={handlePositionChange} />
-            </label>
-
-            <br />
-            <label>
-                打順
-                <input value={battingOrder} onChange={handleBattingOrder} />
-            </label>
-            <h3>
-                今日のあなたのポジションは{position}で打順は{battingOrder}
-                番ですね！
-            </h3>
+            <p>今日のあなたのポジションと打順を選択してください</p>
+            <select value={playPosition} onChange={handlePositionChange}>
+                <option value="ピッチャー">ピッチャー</option>
+                <option value="ライト">ライト</option>
+                <option value="レフト">レフト</option>
+            </select>
+            <h3>今日のあなたのポジションは{playPosition}ですね！</h3>
         </>
     );
 };
