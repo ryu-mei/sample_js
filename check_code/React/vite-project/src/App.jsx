30871e0d65d4ddfda6c02ce6c05ccca15a27bf72
ryumei <ryumei.sakaki06@gmail.com>
2024-05-07 07:09:10 +0900

    filterメソッド 修正

--- check_code/React/vite-project/src/App.jsx
+++ check_code/React/vite-project/src/App.jsx
@@ -1,16 +1,15 @@
 const people = [
   { id: 0, name: 'Murakami', position: 'third baseman' },
-  { id: 1, name: 'Ohtani', positon: 'pitcher' },
-  { id: 2, name: 'Nootbaar', positon: 'outfielder' },
-  { id: 3, name: 'Yoshida', positon: 'outfielder' },
-  { id: 4, name: 'Imanaga', positon: 'pitcher' },
+  { id: 1, name: 'Ohtani', position: 'pitcher' },
+  { id: 2, name: 'Nootbaar', position: 'outfielder' },
+  { id: 3, name: 'Yoshida', position: 'outfielder' },
+  { id: 4, name: 'Imanaga', position: 'pitcher' },
 ];
 
 const List = () => {
-  const fielder = people.filter((person) => (
-    <li>{person.position === 'pitcher'}</li>
-  ));
-  return <ul>{fielder}</ul>;
+  const fielder = people.filter((person) => person.position === 'pitcher');
+  const pitcher = fielder.map((person) => <li>{person.name}</li>);
+  return <ul>{pitcher}</ul>;
 };
 
 export default List;
