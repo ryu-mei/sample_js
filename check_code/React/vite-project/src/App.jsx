cb81585076aaca886820402f0d7f532798a09af2
ryumei <ryumei.sakaki06@gmail.com>
2024-05-06 18:45:40 +0900

    React 条件演算子

--- check_code/React/vite-project/src/App.jsx
+++ check_code/React/vite-project/src/App.jsx
@@ -1,19 +1,16 @@
-import Avatar from './Avatar';
-
-function Card({ children }) {
-  return <div className="card">{children}</div>;
+function Item({ name, isPacked }) {
+  return <li className="item">{isPacked ? <del>{name + '✔️'}</del> : name}</li>;
 }
 
-export default function Profile() {
+export default function PackingList() {
   return (
-    <Card>
-      <Avatar
-        size={100}
-        person={{
-          name: 'Katsuko Saruhashi',
-          imageId: 'YfeOqp2',
-        }}
-      />
-    </Card>
+    <section>
+      <h1>Salliy Ride's Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={true} name="Helmet with a golden leaf" />
+        <Item isPacked={false} name="Photo of Tam" />
+      </ul>
+    </section>
   );
 }
