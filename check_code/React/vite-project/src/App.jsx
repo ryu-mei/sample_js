46da7520cfc1e89d9ef8c3114e91ebca992b551c
ryumei <ryumei.sakaki06@gmail.com>
2024-05-07 21:20:59 +0900

    Recipe

--- check_code/React/vite-project/src/App.jsx
+++ check_code/React/vite-project/src/App.jsx
@@ -1,17 +1,23 @@
-const people = [
-  { id: 0, name: 'Murakami', position: 'third baseman' },
-  { id: 1, name: 'Ohtani', position: 'pitcher' },
-  { id: 2, name: 'Nootbaar', position: 'outfielder' },
-  { id: 3, name: 'Yoshida', position: 'outfielder' },
-  { id: 4, name: 'Imanaga', position: 'pitcher' },
-];
-
-const List = () => {
-  const fielder = people.filter((person) => person.position === 'pitcher');
-  const pitcher = fielder.map((person) => (
-    <li key={person.id}>{person.name}</li>
-  ));
-  return <ul>{pitcher}</ul>;
+const Recipe = ({ drinkers }) => {
+  return (
+    <ol>
+      <li>Boil {drinkers} cups of water.</li>
+      <li>
+        Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.
+      </li>
+      <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
+    </ol>
+  );
 };
 
-export default List;
+const App = () => {
+  return (
+    <section>
+      <h1>Spiced Chai Recipe</h1>
+      <h2>For two</h2>
+      <Recipe drinkers={2} />
+    </section>
+  );
+};
+
+export default App;
