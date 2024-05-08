9b64d30f72cdb08997626478aab07e38355fbcd1
ryumei <ryumei.sakaki06@gmail.com>
2024-05-08 21:10:02 +0900

    React レンダーツリー

--- check_code/React/vite-project/src/App.jsx
+++ check_code/React/vite-project/src/App.jsx
@@ -1,22 +1,15 @@
-const Recipe = ({ drinkers }) => {
-  return (
-    <ol>
-      <li>Boil {drinkers} cups of water.</li>
-      <li>
-        Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.
-      </li>
-      <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
-    </ol>
-  );
-};
+import Copyright from './Copyright.jsx';
+import FancyText from './FancyText.jsx';
+import InspirationGenerator from './InspirationGenerator.jsx';
 
 const App = () => {
   return (
-    <section>
-      <h1>Spiced Chai Recipe</h1>
-      <h2>For two</h2>
-      <Recipe drinkers={2} />
-    </section>
+    <>
+      <FancyText title text="Get Inspired App" />
+      <InspirationGenerator>
+        <Copyright year={2024} />
+      </InspirationGenerator>
+    </>
   );
 };
 
