35bc43df91598ab2fd27dba2030901078b1cee5e
ryumei <ryumei.sakaki06@gmail.com>
2024-05-09 20:56:58 +0900

    React イベント伝播の停止

--- check_code/React/vite-project/src/App.jsx
+++ check_code/React/vite-project/src/App.jsx
@@ -1,8 +1,40 @@
-const Button = () => {
-  const handleClick = () => {
-    alert('クリックされました');
-  };
-  return <button onClick={handleClick}>ボタン</button>;
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+const red = css`
+  color: red;
+`;
+const bgBlue = css`
+  background: #2196f3;
+`;
+
+const Button = (onClick, children) => {
+  return (
+    <button
+      onClick={(e) => {
+        e.stopPropagation();
+        onClick();
+      }}
+    >
+      {children}
+    </button>
+  );
 };
 
-export default Button;
+const Toolbar = () => {
+  return (
+    <div
+      className="Toolbar"
+      onClick={() => {
+        alert('you clicked on the toolbar!');
+      }}
+      css={bgBlue}
+    >
+      <Button onClick={() => alert('playing')} css={red}>
+        play movie
+      </Button>
+      <Button onClick={() => alert('uploading')}>upload image</Button>
+    </div>
+  );
+};
+
+export default Toolbar;
