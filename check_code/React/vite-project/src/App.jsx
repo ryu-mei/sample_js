7becb699bed3a29ecc7ddec86e3524a3a445947e
ryumei <ryumei.sakaki06@gmail.com>
2024-05-19 13:58:16 +0900

    React forwardRef

--- check_code/React/vite-project/src/App.jsx
+++ check_code/React/vite-project/src/App.jsx
@@ -1,75 +1,22 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { useRef } from 'react';
+import { forwardRef, useRef } from 'react';
 
-const displayflex = css`
-  display: flex;
-  list-style: none;
-`;
+const MyInput = forwardRef((props, ref) => {
+  return <input {...props} ref={ref} />;
+});
 
-const CatFriends = () => {
-  const firstCatRef = useRef(null);
-  const secondCatRef = useRef(null);
-  const thirdCatRef = useRef(null);
-
-  function handleScrollToFirstCat() {
-    firstCatRef.current.scrollIntoView({
-      behavior: 'smooth',
-      block: 'nearest',
-      inline: 'center',
-    });
+const Form = () => {
+  const inputRef = useRef(null);
+  function handleClick() {
+    inputRef.current.focus();
   }
-
-  function handleScrollToSecondCat() {
-    secondCatRef.current.scrollIntoView({
-      behavior: 'smooth',
-      block: 'nearest',
-      inline: 'center',
-    });
-  }
-
-  function handleScrollToThirdCat() {
-    thirdCatRef.current.scrollIntoView({
-      behavior: 'smooth',
-      block: 'nearest',
-      inline: 'center',
-    });
-  }
-
   return (
     <>
-      <nav>
-        <button onClick={handleScrollToFirstCat}>猫ちゃん</button>
-        <button onClick={handleScrollToSecondCat}>猫ちゃん</button>
-        <button onClick={handleScrollToThirdCat}>猫ちゃん</button>
-      </nav>
-      <div>
-        <ul css={displayflex}>
-          <li>
-            <img
-              src="https://placekitten.com/g/200/200"
-              alt="猫ちゃん"
-              ref={firstCatRef}
-            />
-          </li>
-          <li>
-            <img
-              src="https://placekitten.com/g/200/200"
-              alt="猫ちゃん"
-              ref={secondCatRef}
-            />
-          </li>
-          <li>
-            <img
-              src="https://placekitten.com/g/200/200"
-              alt="猫ちゃん"
-              ref={thirdCatRef}
-            />
-          </li>
-        </ul>
-      </div>
+      <MyInput ref={inputRef} />
+      <button onClick={handleClick}>Focus the input</button>
     </>
   );
 };
 
-export default CatFriends;
+export default Form;
