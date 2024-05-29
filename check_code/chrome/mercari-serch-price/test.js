4aac04a62a4647a12f804613c5e84b0ad3bb2986
ryumei <ryumei.sakaki06@gmail.com>
2024-05-29 20:03:37 +0900

    titleClassの要素を取得

--- check_code/chrome/mercari-serch-price/test.js
+++ check_code/chrome/mercari-serch-price/test.js
@@ -1,7 +1,13 @@
-const titleClass = document.querySelector('.heading__a7d91561');
-const footerClass = document.querySelector('.sc-85ea0d14-0');
-console.log(titleClass);
-console.log(footerClass);
+setTimeout(() => {
+  const titleClass = document.querySelector('.heading__a7d91561');
+  const footerClass = document.querySelector('.sc-85ea0d14-0');
+  console.log(`test.js 3`, titleClass);
+  console.log(`test.js 4`, footerClass);
+}, 1000);
+
+const titleNull = document.querySelector('.heading__a7d91561');
+console.log(`test.js 5`, titleNull);
+
 {
   /* <h1 class="heading__a7d91561 page__a7d91561">JavaScript 第7版</h1> */
 }
