edd6ec3b4166ab2227702527eed31c3219ade869
ryumei <ryumei.sakaki06@gmail.com>
2024-06-17 07:36:15 +0900

    fetch途中

--- check_code/JsCode/20240613_googleMap/index.js
+++ check_code/JsCode/20240613_googleMap/index.js
@@ -1,20 +1,52 @@
-let map;
+const substringTagAfter = (html, searchTag) => {
+  const index = html.indexOf(searchTag);
+  return html.substring(index + searchTag.length);
+};
 
-async function initMap() {
-  const position = { lat: -25.344, lng: 131.031 };
-  const { Map } = await google.maps.importLibrary('maps');
-  const { AdvancedMarkerView } = await google.maps.importLibrary('marker');
-  map = new Map(document.getElementById('map'), {
-    zoom: 4,
-    center: position,
-    mapId: 'DEMO_MAP_ID',
+const substringTagBefore = (html, searchTag) => {
+  const index = html.indexOf(searchTag);
+  return html.substring(0, index);
+};
+
+(async () => {
+  const res = await fetch('http://localhost:3000/proxy', {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer your-token-here',
+    },
   });
+  console.log(res.json());
 
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: position,
-    title: 'Uluru',
+  const addresses = [];
+  const infomation = [];
+  const lines = htmlPage.split('<HR>'); // テキストを行ごとに分割
+  // console.log(lines);
+
+  for (const line of lines) {
+    const addressAfter = substringTagAfter(line, '分頃、');
+    const addressBefore = substringTagBefore(addressAfter, '付近');
+    const infoAfter = substringTagAfter(line, '【防災かまいし広報】');
+    const infoBefore = substringTagBefore(infoAfter, 'ツキノワグマ');
+    addresses.push(addressBefore);
+    infomation.push(infoBefore);
+  }
+})();
+
+function initMap() {}
+
+function geocodeAddress() {
+  const geocoder = new google.maps.Geocoder();
+  const address = document.getElementById('address').value;
+
+  geocoder.geocode({ address: address }, function (results, status) {
+    if (status === 'OK') {
+      const location = results[0].geometry.location;
+      alert('緯度: ' + location.lat() + ', 経度: ' + location.lng());
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
   });
 }
-
-initMap();
